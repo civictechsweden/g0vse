267fe873d25e5978d59225324d7bb0d41f23d81e
Pierre Mesure <pierre@mesu.re>
2025-03-22 17:47:54 +0000

    Upgrade tailwindcss to v3

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,7 @@
 // tailwind.config.js
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  mode: "jit",
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  variants: {
     extend: {},
   },
   plugins: [],
