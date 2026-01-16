29ffd84b97870d7b41860241887a0a76b46b7d97
Pierre Mesure <pierre@mesu.re>
2026-01-16 21:12:33 +0100

    Upgrade dependencies and add tracking script

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,17 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        data-goatcounter="https://g0vse.goatcounter.com/count"
+        src="//gc.zgo.at/count.js"
+        strategy="afterInteractive"
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
